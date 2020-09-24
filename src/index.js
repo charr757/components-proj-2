18649b084ae48fbb2046478482543c24497285c1
Charlie Spencer <crs992+charr757@gmail.com>
2020-09-24 09:27:09 -0400

    move commentdetail into approval card

--- src/index.js
+++ src/index.js
@@ -7,25 +7,30 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail
-        author="Sam"
-        time="Today at 6:00PM"
-        comment="I was really surprised to find that most of the food was just bologna! One star :/"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Alex"
-        time="Today at 2:23AM"
-        comment="You know, it's like you go in and everyone is screaming!! Really fun time. Would probably go again, but not with my mother."
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Jane"
-        time="Yesterday at 4:00PM"
-        comment="Hands down favorite place in the world. They have amazing cheesecake. Better thank Dunkin Donuts!"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          time="Today at 6:00PM"
+          comment="I was really surprised to find that most of the food was just bologna! One star :/"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          time="Today at 2:23AM"
+          comment="You know, it's like you go in and everyone is screaming!! Really fun time. Would probably go again, but not with my mother."
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          time="Yesterday at 4:00PM"
+          comment="Hands down favorite place in the world. They have amazing cheesecake. Better thank Dunkin Donuts!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 }
